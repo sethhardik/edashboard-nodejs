2acfee6f83ba20c250d315e7aa323f91f27ce668
sethhardik <hseth469@gmail.com>
2022-12-31 18:12:53 +0530

    Nodejs: register api created

---
+++ backend/db/config.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://hardik:hardik@cluster0.jnvelqi.mongodb.net/e-commerce');
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 // import necessary modules here
-const express = require('express')
-
+const express = require('express');
+require("./db/config");
+const User = require("./db/User");
 // initialize app
-const app = express()
+const app = express();
 
-//  creating my 1st route which just prints the message
-app.get("/",(req,resp)=>{
-resp.send("App is working fine.")
+// used to get the json body from api
+app.use(express.json());
+
+app.post("/register",async (req,resp)=>{
+    let user= new User(req.body);
+    let result = await user.save();
+    resp.send(result)
 });
 
 // making app work on 5000 port
